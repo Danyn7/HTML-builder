4399ff7f39a4774ef020b615ecdae1f2d58928b2
Daria Kuzmina <dastjd232323@gmail.com>
2023-05-07 01:03:33 +0300

    feat: add 2 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+const wayNewTextFile = path.join(__dirname, 'writetext.txt');
+const newTextFile = fs.createWriteStream(wayNewTextFile, 'utf-8');
+stdout.write('Hello! Write me something');
+
+stdin.on('data', data =>  {
+    let endGame = "exit";
+    if (Buffer.from(data).toString().trim() === endGame ) {
+        process.exit();
+    } else {
+    newTextFile.write(data);
+    }
+});
+
+process.on('exit', () => stdout.write('Bye for now!'));
+process.on('SIGINT', () => {
+    process.exit();
+});
+
