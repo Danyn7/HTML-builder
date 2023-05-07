41b4c9d54b8a0bcd057d8ad2733710dafc00067a
Daria Kuzmina <dastjd232323@gmail.com>
2023-05-07 22:42:52 +0300

    feat: add task 4

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const wayStyles = path.join(__dirname, 'styles');
+let arr = [];
+
+fs.readdir(wayStyles, (err, files) => {
+  if (err) {
+    console.error(err);
+  }
+    
+  files.forEach(file => {
+    fs.stat(path.join(__dirname, 'styles', file), (errs, stats) => {
+      if (stats.isFile()) {
+        if (errs) {
+          console.error(errs);
+          return;
+        } else if (path.extname(file) === '.css') {
+          fs.readFile(path.join(__dirname, 'styles', file), (err, file) => {
+            if (err) {
+              console.error(err);
+            }
+            arr.push(Buffer.from(file).toString().trim()); 
+            
+            fs.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), arr.join(''), (err) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+                
+            });
+          });
+        
+        }
+    
+      }});});});
